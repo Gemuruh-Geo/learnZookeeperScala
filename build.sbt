a16895921febae0738676f35969badb176ba1182
Gemuruh Geo <g.geo@kereta-api.co.id>
2021-06-04 10:43:51 +0700

    inital commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "learnZookeeper"
+
+version := "0.1"
+
+scalaVersion := "2.13.6"
---
+++ src/main/scala/com/geo/Main.scala
@@ -0,0 +1,9 @@
+package com.geo
+
+/**
+ * @author Gemuruh Geo Pratama
+ * @created 04/06/2021-10:43 AM
+ */
+object Main extends App {
+
+}
